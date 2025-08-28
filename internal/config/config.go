fc2f492eeb8d44aa4eb6e220e1ad829bc98463e5
Guilherme Freire <gfreire1992@gmail.com>
2025-08-28 19:28:29 -0300

    chore: refactor path in config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,22 +6,35 @@ import (
 	"os"
 )
 
+const (
+	configFile = "/.gatorconfig.json"
+)
+
 type Config struct {
 	DbUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
+func getConfigPath() (string, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("UserHomeDir not found")
+	}
+
+	fullPath := homePath + configFile
+
+	return fullPath, nil
+}
+
 func Read() (Config, error) {
 	empty := Config{}
 
-	homePath, err := os.UserHomeDir()
+	path, err := getConfigPath()
 	if err != nil {
-		return empty, fmt.Errorf("UserHomeDir not found")
+		return empty, fmt.Errorf("error getting path: %v", err)
 	}
 
-	fullPath := homePath + "/.gatorconfig.json"
-
-	contentBytes, err := os.ReadFile(fullPath)
+	contentBytes, err := os.ReadFile(path)
 	if err != nil {
 		return empty, fmt.Errorf("error reading file: %v", err)
 	}
