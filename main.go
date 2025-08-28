a66402de62d7bbe633153c9f6d4e0d6034840321
Guilherme Freire <gfreire1992@gmail.com>
2025-08-28 19:10:50 -0300

    chore: add test on main()

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/gfrei/gator-cli/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(cfg)
+	}
 }
