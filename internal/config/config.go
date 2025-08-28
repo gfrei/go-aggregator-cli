209d978fdf7916a1bf2e17e741235e2c38412c79
Guilherme Freire <gfreire1992@gmail.com>
2025-08-28 19:05:05 -0300

    feat: add logic to config.Read()

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,35 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	DbUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() Config {
-	return Config{}
+func Read() (Config, error) {
+	empty := Config{}
+
+	path, err := os.UserHomeDir()
+	if err != nil {
+		return empty, fmt.Errorf("UserHomeDir not found")
+	}
+
+	contentBytes, err := os.ReadFile(path)
+	if err != nil {
+		return empty, fmt.Errorf("error reading file: %v", err)
+
+	}
+
+	var parsed Config
+	err = json.Unmarshal(contentBytes, &parsed)
+	if err != nil {
+		return empty, fmt.Errorf("could not parse json")
+	}
+
+	return parsed, nil
 }
