cc8fdea8100b97a4e99deb6e54471b66512fcd77
Guilherme Freire <gfreire1992@gmail.com>
2025-08-29 20:41:01 -0300

    feat: add handlerLogin to main

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gfrei/gator-cli/internal/config"
+)
+
 type state struct {
 	config *config.Config
 }
@@ -9,6 +15,22 @@ type command struct {
 	args []string
 }
 
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login error: no username")
+	}
+
+	user := cmd.args[0]
+
+	err := s.config.SetUser(user)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Login as:", user)
+
+	return nil
+}
 
 func main() {
 }
