1e9de9853a866c596a294112f4d29e135a018b6c
Guilherme Freire <gfreire1992@gmail.com>
2025-08-29 20:41:12 -0300

    chore: add main_test.go

---
+++ main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gfrei/gator-cli/internal/config"
+)
+
+func TestHandlerLogin(t *testing.T) {
+	config := config.New()
+
+	state := state{
+		config: &config,
+	}
+
+	cmd := command{
+		name: "login",
+		args: []string{"gfrei"},
+	}
+
+	err := handlerLogin(&state, cmd)
+
+	if err != nil {
+		t.Errorf("")
+		return
+	} else {
+		t.Log("Ok!")
+	}
+}
