ba518ef39f651951fdaf0122a9ea014b6b3024ee
Guilherme Freire <gfreire1992@gmail.com>
2025-09-02 19:26:13 -0300

    chore: refactor handler functions to commandHandlers.go

---
+++ commandHandlers.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gfrei/gator-cli/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+)
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login error: no username")
+	}
+
+	user := cmd.args[0]
+
+	return setUser(s, user)
+}
+
+func setUser(s *state, username string) error {
+	_, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("user %q not registered", username)
+	}
+
+	err = s.config.SetUser(username)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("logged in as: %q\n", username)
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("register error: no username")
+	}
+
+	fullName := (strings.Join(cmd.args, " "))
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      fullName,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("registered user %q\n", user.Name)
+
+	return setUser(s, user.Name)
+}
+
+func handlerReset(s *state, cmd command) error {
+	count, err := s.db.CountUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	err = s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("deleted %v users\n", count)
+
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetAllUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if user.Name != s.config.CurrentUserName {
+			fmt.Printf("* %v\n", user.Name)
+		} else {
+			fmt.Printf("* %v (current)\n", user.Name)
+		}
+	}
+
+	return nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	// if len(cmd.args) == 0 {
+	// 	return fmt.Errorf("agg error: add an url")
+	// }
+
+	// feed, err := fetchFeed(context.Background(), cmd.args[0])
+	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(feed)
+
+	return nil
+}
