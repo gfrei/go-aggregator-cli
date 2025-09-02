91e166c4681384953665ff744f583230979e907a
Guilherme Freire <gfreire1992@gmail.com>
2025-09-02 12:20:53 -0300

    feat: add handlerRegister

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/gfrei/gator-cli/internal/database"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
@@ -14,6 +19,10 @@ func handlerLogin(s *state, cmd command) error {
 
 	user := cmd.args[0]
 
+	return setUser(s, user)
+}
+
+func setUser(s *state, user string) error {
 	err := s.config.SetUser(user)
 	if err != nil {
 		return err
@@ -22,6 +31,29 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Println("Login as:", user)
 
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("register error: no username")
+	}
+
+	fullName := (strings.Join(cmd.args, " "))
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      fullName,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("register user %q\n", user.Name)
+
+	return setUser(s, user.Name)
 }
 
 func main() {
@@ -34,6 +66,7 @@ func main() {
 	_commands := initCommands()
 
 	_commands.register("login", handlerLogin)
+	_commands.register("register", handlerRegister)
 
 	err = processCommand(&_state, &_commands, os.Args[1:])
 	if err != nil {
